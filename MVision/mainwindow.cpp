67ef119dce0af7bb975c54cba4e639267cee6efb
HKrecki <hubertkrecki@gmail.com>
2022-02-13 09:42:13 +0100

    Added screenshot and save function

--- MVision/mainwindow.cpp
+++ MVision/mainwindow.cpp
@@ -6,6 +6,13 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    foreach (QScreen *screen, QGuiApplication::screens()) {
+        ui->cmbScreens->addItem(screen->name());
+    }
+
+    ui->btnSave->setEnabled(false);
+    ui->LblScreen->setText("");
 }
 
 MainWindow::~MainWindow()
@@ -16,10 +23,53 @@ MainWindow::~MainWindow()
 
 void MainWindow::on_btnScreen_clicked()
 {
+    QScreen *screen = QGuiApplication::screens().at(ui->cmbScreens->currentIndex());
+    if(!screen){
+        QMessageBox::critical(this,"Error", "Could not get the screen!");
+        return;
+    }
 
+    hide();
+    QTimer::singleShot(500, this, &MainWindow::shoot);
 }
 
 void MainWindow::on_btnSave_clicked()
 {
+    QString path = QFileDialog::getSaveFileName(this, "Save", QString(), "PNG image (*.png)");
+    if(path.isEmpty())  return;
 
+    QFileInfo fi(path);
+    if(fi.exists()){
+        QMessageBox::StandardButton btn = QMessageBox::question(this, "Exists", "File already exists, overwrite?");
+        if(btn != QMessageBox::StandardButton::Yes) return;
+    }
+
+    if(!m_image.save(path, "PNG")){
+        QMessageBox::critical(this,"Error", "Could not save image");
+    }
 }
+
+void MainWindow::shoot()
+{
+    QScreen *screen = QGuiApplication::screens().at(ui->cmbScreens->currentIndex());
+
+    m_image = screen->grabWindow(0);
+
+    QPixmap m_scaled = m_image.scaled(ui->LblScreen->width(), ui->LblScreen->height(), Qt::AspectRatioMode::KeepAspectRatio);
+
+    ui->LblScreen->setPixmap(m_scaled);
+    ui->btnSave->setEnabled(true);
+    show();
+}
+
+
+
+
+
+
+
+
+
+
+
+
--- MVision/mainwindow.h
+++ MVision/mainwindow.h
@@ -5,6 +5,10 @@
 #include <QScreen>
 #include <QMessageBox>
 #include <QPixmap>
+#include <QDialog>
+#include <QFileInfo>
+#include <QFileDialog>
+#include <QTimer>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -20,10 +24,13 @@ public:
 
 private slots:
     void on_btnScreen_clicked();
-
     void on_btnSave_clicked();
+    void shoot();
 
 private:
     Ui::MainWindow *ui;
+    QPixmap m_image;
+    QTimer m_timer;
+
 };
 #endif // MAINWINDOW_H
